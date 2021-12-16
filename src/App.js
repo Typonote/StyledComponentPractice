0cb153626228ea9083640940cf4a8f146ffce0a7
JinKyung Kim <kimradium@naver.com>
2021-12-17 00:22:36 +0900

    #2 Our First Styled Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import "./App.css";
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
 
 function App() {
-  return null;
+  return (
+    <Father>
+      <BoxOne>
+        <Text>One</Text>
+      </BoxOne>
+      <BoxTwo>
+        <Text>Two</Text>
+      </BoxTwo>
+    </Father>
+  );
 }
 
 export default App;
